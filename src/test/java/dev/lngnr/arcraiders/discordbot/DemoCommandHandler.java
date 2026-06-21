e78400f73eee269447fca3d6bf8ff8f724631a9a
Jonas Eddy Langner <jonas.langner23@gmail.com>
2026-06-21 09:07:12 +0000

    tests

---
+++ src/test/java/dev/lngnr/arcraiders/discordbot/DemoCommandHandler.java
@@ -0,0 +1,21 @@
+package dev.lngnr.arcraiders.discordbot;
+
+import dev.lngnr.arcraiders.discordbot.discord.boundary.CommandHandler;
+import dev.lngnr.arcraiders.discordbot.discord.boundary.CommandMapping;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+@CommandMapping(name = "demo")
+public class DemoCommandHandler implements CommandHandler {
+
+    private int calls = 0;
+
+    @Override
+    public void handle(SlashCommandInteractionEvent event) {
+        ++calls;
+    }
+
+    public int getCalls() {
+        return calls;
+    }
+
+}
