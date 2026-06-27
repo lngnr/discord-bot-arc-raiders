2509e92d93ede47f55d31b40cf401665aa7ff5df
Jonas Eddy Langner <jonas.langner23@gmail.com>
2026-06-27 16:43:15 +0000

    fix(ci): no bot token needed

--- src/test/java/dev/lngnr/arcraiders/discordbot/ArcRaidersDiscordBotApplicationTests.java
+++ src/test/java/dev/lngnr/arcraiders/discordbot/ArcRaidersDiscordBotApplicationTests.java
@@ -1,12 +1,8 @@
 package dev.lngnr.arcraiders.discordbot;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.modulith.core.ApplicationModules;
 
-import dev.lngnr.arcraiders.discordbot.configuration.JDATestConfiguration;
-
-@SpringBootTest(classes = JDATestConfiguration.class)
 class ArcRaidersDiscordBotApplicationTests {
 
     @Test
